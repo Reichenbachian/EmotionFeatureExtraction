5970d300b14d455e1dc26be126c55738a3ed966b
localhost <greichenbach@andover.edu>
2018-01-10 19:34:14 -0500

    Updating experiments and nn architectures

--- nets.py
+++ nets.py
@@ -4,39 +4,50 @@ from keras import backend as K
 from keras import regularizers
 from keras.models import Sequential
 from keras.layers import Dense, Activation, LSTM, Dropout, Flatten, Conv1D, BatchNormalization, MaxPooling1D, Reshape
-from config import config as cfg
 from keras import regularizers as reg
 from dlframework.net_factory.network import Network
 import attr
 import pdb
 import keras.backend.tensorflow_backend as K
+from adlframework.nets.net import Net
 
 
-@attr.s
-class audio_feature_extractor(Network):
-    num_classes = attr.ib()
-    input_shape = attr.ib()
-    transfer = attr.ib(default=False)
-    
+class audio_feature_extractor(Net):
+    @Net.build_model_wrapper
     def build_model(self, mode=False):
-        with tf.device('/cpu:0'):
-            PADDING = 'valid'
-            print "Input shape prior to processing via 1st conv layer: ", self.input_shape
-            model=Sequential()
-            model.add(Conv1D(20, 80, activation='relu', input_shape=self.input_shape))
-            model.add(MaxPooling1D(8))
-            model.add(Conv1D(32, 40, strides=2, activation='relu'))
-            model.add(MaxPooling1D(8))
-            model.add(Conv1D(64, 32, strides=1, activation='relu'))
-            model.add(MaxPooling1D(8))
-            model.add(Conv1D(128, 16, strides=2, activation='relu'))
-            model.add(MaxPooling1D(4))
-            model.add(Conv1D(256, 4, strides=1, activation='relu'))
-            model.add(Dropout(.5))
+        PADDING = 'valid'
+        model=Sequential()
+        model.add(Conv1D(20, 80, activation='relu', input_shape=self.input_shape))
+        model.add(MaxPooling1D(8))
+        model.add(Conv1D(32, 40, strides=2, activation='relu'))
+        model.add(MaxPooling1D(8))
+        model.add(Conv1D(64, 32, strides=1, activation='relu'))
+        model.add(MaxPooling1D(8))
+        model.add(Conv1D(128, 16, strides=2, activation='relu'))
+        model.add(MaxPooling1D(4))
+        model.add(Conv1D(256, 4, strides=1, activation='relu'))
+        model.add(Dropout(.5))
 
-            model.add(Activation('softmax', name = 'audio_feature_extractor'))
+        model.add(Activation('softmax', name = 'audio_feature_extractor'))
 
 
-            print "Model Summary:"
-            print model.summary()    
-            return model
+        print "Model Summary:"
+        print model.summary()    
+        return model
+
+class dense_network(Net):
+
+    @Net.build_model_wrapper
+    def build_model(self):
+        model=Sequential()
+        model.add(Dense(512, activation='relu', input_shape=self.input_shape))
+        model.add(Dense(256, activation='relu', input_shape=self.input_shape))
+        model.add(Dense(128, activation='relu', input_shape=self.input_shape))
+        model.add(Dense(64, activation='relu', input_shape=self.input_shape))
+        model.add(Dense(self.target_shape, activation='relu'))
+        model.add(Activation('softmax', name = 'audio_feature_extractor'))
+
+
+        print "Model Summary:"
+        print model.summary()    
+        return model
